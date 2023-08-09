131d66823d93fa43848041bb4ef5c95d92134cf9
ivi0540 <ivi0540@gmail.com>
2023-08-09 22:45:37 +0300

    feat: save1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
 }
+
+
+function check(str, bracketsConfig) {
+  let openBrace = "";
+  let closeBrace = "";
+  let examination = true;
+  if (str.length % 2 === 0) {
+    for (brackets of bracketsConfig) {
+      openBrace = brackets[0];
+      closeBrace = brackets[1];
+
+      // while (str.indexOf(closeBrace) !== -1) {
+      //     console.log(str.indexOf(closeBrace));
+      // };
+
+    };
+  } else {
+    examination = false;
+  };
+}
+console.log(check('([{}])', [['(', ')'], ['[', ']'], ['{', '}']]));
+
+// check('[(])', [['(', ')'], ['[', ']']]) // -> false
